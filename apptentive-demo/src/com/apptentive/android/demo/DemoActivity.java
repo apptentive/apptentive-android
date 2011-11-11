dc819659ac2a594d3dfb13f9f68568e332053999
Sky Kelsey <sky.kelsey@gmail.com>
2011-11-11 11:15:38 -0800

    Update the client to point to production Apptentive. Remove our testing API key from checked in code to avoid confusion.

--- apptentive-demo/src/com/apptentive/android/demo/DemoActivity.java
+++ apptentive-demo/src/com/apptentive/android/demo/DemoActivity.java
@@ -7,19 +7,21 @@ import android.view.View;
 import android.widget.Button;
 import com.apptentive.android.sdk.Apptentive;
 
+import java.util.HashMap;
+
 public class DemoActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.e("DEMO", "onCreate()");
 		setContentView(R.layout.main);
 
-		//final Apptentive apptentive = Apptentive.initialize(this, "Demo Activity", "0d7c775a973b30ed6a8ca2cf6469af3168a8c5e38ccd26755d1fdaa3387c6454", 5, 10, 5, 4); // api.apptentive.com
-		final Apptentive apptentive = Apptentive.initialize(this, "Demo Activity", "7a9974bf16d7b2c354f2eb2fca612c25fdab62aebeeb3f80767c70e53b35cbd9", 5, 10, 5, 4);   //api.apptentive-beta.com
+		final Apptentive apptentive = Apptentive.initialize(this, "Demo Activity", "<YOUR_API_KEY>", 5, 10, 5, 4);
 
 		// Add custom data fields to feedback this way:
-		//HashMap<String, String> customFeedbackFields = new HashMap<String, String>();
-		//customFeedbackFields.put("foo", "bar");
-		//apptentive.addFeedbackDataFields(customFeedbackFields);
+		HashMap<String, String> customFeedbackFields = new HashMap<String, String>();
+		customFeedbackFields.put("aqua_pet_version", "1.0.2");
+		customFeedbackFields.put("aqua_pet_id", "0123456789ABCDEF");
+		apptentive.addFeedbackDataFields(customFeedbackFields);
 
 		Button resetButton = (Button) findViewById(R.id.button_reset);
 		resetButton.setOnClickListener(new View.OnClickListener() {
