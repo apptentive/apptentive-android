3b13159f286e720ec653423341116d2d8c3c617a
Sky Kelsey <sky.kelsey@gmail.com>
2012-01-19 00:55:36 -0800

    Send network_type as String, instead of int. Fix feedback so new feedback is used each time. [#APPTENTIVE-187]

--- apptentive-android-sdk/src/com/apptentive/android/sdk/util/Constants.java
+++ apptentive-android-sdk/src/com/apptentive/android/sdk/util/Constants.java
@@ -76,4 +76,32 @@ public class Constants {
 		animation.setInterpolator(SCREEN_ANIMATION_INTERPOLATOR);
 		return animation;
 	}
+
+	private static final String[] networkTypeLookup = {
+			"NETWORK_TYPE_UNKNOWN", // 0
+			"NETWORK_TYPE_GPRS",    //  1
+			"NETWORK_TYPE_EDGE",    //  2
+			"NETWORK_TYPE_UMTS",    //  3
+			"NETWORK_TYPE_CDMA",    //  4
+			"NETWORK_TYPE_EVDO_0",  //  5
+			"NETWORK_TYPE_EVDO_A",  //  6
+			"NETWORK_TYPE_1xRTT",   //  7
+			"NETWORK_TYPE_HSDPA",   //  8
+			"NETWORK_TYPE_HSUPA",   //  9
+			"NETWORK_TYPE_HSPA",    // 10
+			"NETWORK_TYPE_IDEN",    // 11
+			"NETWORK_TYPE_EVDO_B",  // 12
+			"NETWORK_TYPE_LTE",     // 13
+			"NETWORK_TYPE_EHRPD",   // 14
+			"NETWORK_TYPE_HSPAP"    // 15
+	};
+
+	public static String networkTypeAsString(int networkTypeAsInt) {
+		try {
+			return networkTypeLookup[networkTypeAsInt];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return networkTypeLookup[0];
+		}
+	}
+
 }
