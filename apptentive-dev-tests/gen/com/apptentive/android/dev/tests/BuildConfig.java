552c809716f6c770fe7b87750508a67bcf861813
skykelsey <sky.kelsey@gmail.com>
2012-11-18 16:00:25 -0800

    Remove temorary files.

--- apptentive-dev-tests/gen/com/apptentive/android/dev/tests/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.apptentive.android.dev.tests;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- apptentive-dev-tests/gen/com/apptentive/android/dev/tests/R.java
+++
@@ -1,5 +0,0 @@
-package com.apptentive.android.dev.tests;
-
-/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
-public final class R {
-}
