6f35097763068a4f111873e6d9ae4a1ff2c4a8f4
skykelsey <sky.kelsey@gmail.com>
2014-01-22 14:36:44 -0800

    Fix formatting from pull.

--- apptentive-android-sdk/src/com/apptentive/android/sdk/util/Util.java
+++ apptentive-android-sdk/src/com/apptentive/android/sdk/util/Util.java
@@ -205,15 +205,15 @@ public class Util {
 		ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 		return cm != null && cm.getActiveNetworkInfo() != null;
 	}
-	
+
 	public static void ensureClosed(Closeable stream) {
-      if (stream != null) {
-         try {
-            stream.close();
-         } catch (IOException e) {
-         }
-      }
-   }
+		if (stream != null) {
+			try {
+				stream.close();
+			} catch (IOException e) {
+			}
+		}
+	}
 
 	public static Point getScreenSize(Context context) {
 		Point ret = new Point();
@@ -283,7 +283,7 @@ public class Util {
 	/**
 	 * <p>This method will allow you to pass in literal strings. You must wrap the string in single quotes in order to ensure it is not modified
 	 * by Android. Android will try to coerce the string to a float, Integer, etc., if it looks like one.</p>
-	 *
+	 * <p/>
 	 * <p>Example: <code>&lt;meta-data android:name="sdk_distribution" android:value="'1.00'"/></code></p>
 	 * <p>This will evaluate to a String "1.00". If you leave off the single quotes, this method will just cast to a String, so the result would be a String "1.0".</p>
 	 */
