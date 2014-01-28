1ea8dcd7416a54ae3c5eb5e76defdd2bd6035532
skykelsey <sky.kelsey@gmail.com>
2014-01-28 12:17:55 -0800

    Modified project files for IntelliJ 13. Added another Upgrade Message demo.

--- apptentive-dev/src/com/apptentive/android/dev/TestsActivity.java
+++ apptentive-dev/src/com/apptentive/android/dev/TestsActivity.java
@@ -193,19 +193,37 @@ public class TestsActivity extends ApptentiveActivity {
 					"  }\n" +
 					"}";
 
+	private static final String UPGRADE_MESSAGE_REAL_EXAMPLE =
+			"{\n" +
+					"    \"id\": \"52e8091f7724c5cf1f00007b\",\n" +
+					"    \"priority\": 2,\n" +
+					"    \"criteria\": {\n" +
+					"        \"code_point/app.launch/invokes/version\": 1,\n" +
+					"        \"application_version\": \"4.1\"\n" +
+					"    },\n" +
+					"    \"type\": \"UpgradeMessage\",\n" +
+					"    \"configuration\": {\n" +
+					"        \"show_app_icon\": true,\n" +
+					"        \"show_powered_by\": true,\n" +
+					"        \"body\": \"<html><head><style>\\nbody {\\n\\tfont-family: \\\"Helvetica Neue\\\", Helvetica;\\n\\tcolor: #4d4d4d;\\n\\tfont-size: .875em;\\n\\tline-height: 1.36em;\\n\\t-webkit-text-size-adjust:none;\\n}\\n\\nh1, h2, h3, h4, h5, h6 {\\n\\tcolor: #000000;\\n\\tline-height: 1.25em;\\n\\ttext-align: center;\\n}\\n\\nh1 {font-size: 22px;}\\nh2 {font-size: 18px;}\\nh3 {font-size: 16px;}\\nh4 {font-size: 14px;}\\nh5, h6 {font-size: 12px;}\\nh6 {font-weight: normal;}\\n\\nblockquote {\\n\\tmargin: 1em 1.75em;\\n\\tfont-style: italic;\\n}\\n\\nul, ol {\\n\\tpadding-left: 1.75em;\\n}\\n\\ntable {\\n\\tborder-collapse: collapse;\\n\\tborder-spacing: 0;\\n\\tempty-cells: show;\\n}\\n\\ntable caption {\\n\\tpadding: 1em 0;\\n\\ttext-align: center;\\n}\\n\\ntable td,\\ntable th {\\n\\tborder-left: 1px solid #cbcbcb;\\n\\tborder-width: 0 0 0 1px;\\n\\tfont-size: inherit;\\n\\tmargin: 0;\\n\\tpadding: .25em .5em;\\n\\n}\\ntable td:first-child,\\ntable th:first-child {\\n\\tborder-left-width: 0;\\n}\\ntable th:first-child {\\n\\tborder-radius: 4px 0 0 4px;\\n}\\ntable th:last-child {\\n\\tborder-radius: 0 4px 4px 0;\\n}\\n\\ntable thead {\\n\\tbackground: #E5E5E5;\\n\\tcolor: #000;\\n\\ttext-align: left;\\n\\tvertical-align: bottom;\\n}\\n\\ntable td {\\n\\tbackground-color: transparent;\\n\\tborder-bottom: 1px solid #E5E5E5;\\n}\\n</style></head><body><p style=\\\"text-align: center;\\\"><strong>Aptentive SDK</strong></p><hr><p>New in version 1.2.8:</p><ul><li>A new system for sending engagements to the user.</li><li>A new engagement: Upgrade Messages!</li><li>Support for Push Notifications through Urban Airship.</li><li>Several bug fixes, and a nicer looking dev app.</li></ul><p>Enjoy!</p></body></html>\"\n" +
+					"    }\n" +
+					"}";
+
+
 	public void interaction(@SuppressWarnings("unused") View view) {
 		Spinner interactionsSpinner = (Spinner) findViewById(R.id.interaction_spinner);
 		String interactionName = (String) interactionsSpinner.getSelectedItem();
 		Log.e("Testing engage(%s)", interactionName);
 		long start = System.currentTimeMillis();
 		Apptentive.engage(this, interactionName);
-		// TODO: Make this actually use the spinner.
 		try {
 			Interaction interaction = null;
-			if (interactionName.equals("UpgradeMessage Branding")) {
+			if (interactionName.equals("UpgradeMessage exercise with branding")) {
 				interaction = new UpgradeMessageInteraction(UPGRADE_MESSAGE_BRANDING_INTERACTION);
-			} else if (interactionName.equals("UpgradeMessage No Branding")) {
+			} else if (interactionName.equals("UpgradeMessage exercise no branding")) {
 				interaction = new UpgradeMessageInteraction(UPGRADE_MESSAGE_NO_BRANDING_INTERACTION);
+			} else if (interactionName.equals("UpgradeMessage real example")) {
+				interaction = new UpgradeMessageInteraction(UPGRADE_MESSAGE_REAL_EXAMPLE);
 			}
 			if (interaction != null) {
 				EngagementModule.launchInteraction((Activity) view.getContext(), interaction);
