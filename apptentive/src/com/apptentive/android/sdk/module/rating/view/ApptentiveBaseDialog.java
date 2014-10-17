2529c80a1b5c3f18dad9790a39d878bc77a20eab
skykelsey <sky.kelsey@gmail.com>
2014-10-17 16:22:13 -0700

    Dim background when displaying a dialog.

--- apptentive/src/com/apptentive/android/sdk/module/rating/view/ApptentiveBaseDialog.java
+++ apptentive/src/com/apptentive/android/sdk/module/rating/view/ApptentiveBaseDialog.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Apptentive, Inc. All Rights Reserved.
+ * Copyright (c) 2014, Apptentive, Inc. All Rights Reserved.
  * Please refer to the LICENSE file for the terms and conditions
  * under which redistribution and use of this file is permitted.
  */
@@ -8,7 +8,9 @@ package com.apptentive.android.sdk.module.rating.view;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.view.Gravity;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
@@ -26,7 +28,11 @@ public abstract class ApptentiveBaseDialog extends Dialog {
 		// Let the dialog take up the whole device width.
 		WindowManager.LayoutParams params = getWindow().getAttributes();
 		params.width = ViewGroup.LayoutParams.FILL_PARENT;
+		params.height = ViewGroup.LayoutParams.FILL_PARENT;
+		params.gravity = Gravity.CENTER;
+		params.dimAmount = 0.5f;
 		getWindow().setAttributes(params);
-		getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+		getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
 	}
 }
