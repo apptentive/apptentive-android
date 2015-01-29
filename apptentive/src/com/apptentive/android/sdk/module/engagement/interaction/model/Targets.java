79caa44418e362571e52a992bd28a59c890dcdc7
skykelsey <sky.kelsey@gmail.com>
2015-01-28 19:10:24 -0800

    Reduce noisy log level.

--- apptentive/src/com/apptentive/android/sdk/module/engagement/interaction/model/Targets.java
+++ apptentive/src/com/apptentive/android/sdk/module/engagement/interaction/model/Targets.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Apptentive, Inc. All Rights Reserved.
+ * Copyright (c) 2015, Apptentive, Inc. All Rights Reserved.
  * Please refer to the LICENSE file for the terms and conditions
  * under which redistribution and use of this file is permitted.
  */
@@ -40,7 +40,7 @@ public class Targets extends JSONObject {
 				}
 			}
 		}
-		Log.e("No runnable Interactions for EventLabel: %s", eventLabel);
+		Log.v("No runnable Interactions for EventLabel: %s", eventLabel);
 		return null;
 	}
 }
