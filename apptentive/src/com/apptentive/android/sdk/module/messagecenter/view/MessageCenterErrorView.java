1d4ad71b44e7749f3d4bc9760fcdf4b769c519ea
skykelsey <sky.kelsey@gmail.com>
2015-07-07 12:28:54 -0700

    Hook Message Center Error Message up to the state of the last error. Add strings. Add drawables. Adjust styles. AND-161

--- apptentive/src/com/apptentive/android/sdk/module/messagecenter/view/MessageCenterErrorView.java
+++ apptentive/src/com/apptentive/android/sdk/module/messagecenter/view/MessageCenterErrorView.java
@@ -7,12 +7,17 @@
 package com.apptentive.android.sdk.module.messagecenter.view;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.apptentive.android.sdk.R;
+import com.apptentive.android.sdk.util.Constants;
 
 /**
  * @author Sky Kelsey
@@ -31,8 +36,19 @@ public class MessageCenterErrorView extends FrameLayout {
 		back.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				activity.finish();
+				MessageCenterErrorView.this.activity.finish();
 			}
 		});
+
+		SharedPreferences prefs = getContext().getSharedPreferences(Constants.PREF_NAME, Context.MODE_PRIVATE);
+		boolean serverErrorLastAttempt = prefs.getBoolean(Constants.PREF_KEY_MESSAGE_CENTER_SERVER_ERROR_LAST_ATTEMPT, false);
+		if (serverErrorLastAttempt) {
+			((ImageView) findViewById(R.id.icon)).setImageResource(R.drawable.apptentive_icon_server_error);
+			((TextView) findViewById(R.id.message)).setText(R.string.apptentive_message_center_server_error);
+		} else {
+			((ImageView) findViewById(R.id.icon)).setImageResource(R.drawable.apptentive_icon_no_connection);
+			((TextView) findViewById(R.id.message)).setText(R.string.apptentive_message_center_no_connection);
+		}
+
 	}
 }
