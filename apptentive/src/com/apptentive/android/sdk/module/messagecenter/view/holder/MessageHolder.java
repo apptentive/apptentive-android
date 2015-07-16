1df345b0537474e4b8d761a92d3eb1b3d0ade9a4
skykelsey <sky.kelsey@gmail.com>
2015-07-16 16:37:16 -0700

    Check that datestamp and status fields exist before setting them, since incoming messages don't have a status view.

--- apptentive/src/com/apptentive/android/sdk/module/messagecenter/view/holder/MessageHolder.java
+++ apptentive/src/com/apptentive/android/sdk/module/messagecenter/view/holder/MessageHolder.java
@@ -25,9 +25,13 @@ public class MessageHolder extends MessageCenterListItemHolder {
 	}
 
 	public void updateMessage(String datestamp, String status) {
-		this.datestamp.setText(datestamp);
-		this.datestamp.setVisibility(datestamp != null ? View.VISIBLE : View.GONE);
-		this.status.setText(status);
-		this.status.setVisibility(status != null ? View.VISIBLE : View.GONE);
+		if (this.datestamp != null) {
+			this.datestamp.setText(datestamp);
+			this.datestamp.setVisibility(datestamp != null ? View.VISIBLE : View.GONE);
+		}
+		if (this.status != null) {
+			this.status.setText(status);
+			this.status.setVisibility(status != null ? View.VISIBLE : View.GONE);
+		}
 	}
 }
