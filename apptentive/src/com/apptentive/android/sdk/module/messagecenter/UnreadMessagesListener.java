b67a363c2709b18d3b067da2697c2c68785dedb2
skykelsey <sky.kelsey@gmail.com>
2015-09-04 01:08:53 -0700

    Update javadoc.

--- apptentive/src/com/apptentive/android/sdk/module/messagecenter/UnreadMessagesListener.java
+++ apptentive/src/com/apptentive/android/sdk/module/messagecenter/UnreadMessagesListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Apptentive, Inc. All Rights Reserved.
+ * Copyright (c) 2015, Apptentive, Inc. All Rights Reserved.
  * Please refer to the LICENSE file for the terms and conditions
  * under which redistribution and use of this file is permitted.
  */
@@ -8,15 +8,17 @@ package com.apptentive.android.sdk.module.messagecenter;
 
 /**
  * This interface allows you to receive a notification when the number of unread messages changes.
+ *
  * @author Sky Kelsey
  */
 public interface UnreadMessagesListener {
 	/**
-	 * This method is called if the number changes, increasing or decreasing. Therefore, it will be called when new
-	 * messages are available to be viewed, as well as when a message is viewed for the first time. You can use the count
-	 * returned to decorate a view with a badge indicating how many unread messages are waiting to be seen.
+	 * This method is called if the number of unread messages waiting to be viewed by your customer
+	 * increases or decreases. Therefore, it will be called when new messages are available to be
+	 * viewed, as well as when a message is viewed for the first time. You can use the count returned
+	 * to decorate a view with a badge indicating how many unread messages are waiting to be seen.
 	 *
 	 * @param unreadMessages The total number of unread messages waiting to be viewed by the user.
 	 */
-	public void onUnreadMessageCountChanged(int unreadMessages);
+	void onUnreadMessageCountChanged(int unreadMessages);
 }
