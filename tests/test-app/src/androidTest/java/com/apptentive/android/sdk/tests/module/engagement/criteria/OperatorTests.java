86e50f012c90205ea131340f1cc5900af3c37cc3
skykelsey <sky.kelsey@gmail.com>
2015-11-17 14:19:53 -0800

    Refactor `OperatorTests` to remove redundancy.

--- tests/test-app/src/androidTest/java/com/apptentive/android/sdk/tests/module/engagement/criteria/OperatorTests.java
+++ tests/test-app/src/androidTest/java/com/apptentive/android/sdk/tests/module/engagement/criteria/OperatorTests.java
@@ -25,85 +25,52 @@ public class OperatorTests extends ApptentiveInstrumentationTestCase {
 	public void testOperatorExists() {
 		Log.e("Running test: testOperatorExists()\n\n");
 		resetDevice();
-
-		String json = loadFileAssetAsString(TEST_DATA_DIR + "testOperatorExists.json");
-		try {
-			InteractionCriteria criteria = new InteractionCriteria(json);
-			assertTrue(criteria.isMet(getTargetContext()));
-		} catch (JSONException e) {
-			Log.e("Error parsing test JSON.", e);
-			assertNull(e);
-		}
+		doTest("testOperatorExists.json");
 		Log.e("Finished test.");
 	}
 
 	public void testOperatorNot() {
 		Log.e("Running test: testOperatorNot()\n\n");
 		resetDevice();
-
-		String json = loadFileAssetAsString(TEST_DATA_DIR + "testOperatorNot.json");
-		try {
-			InteractionCriteria criteria = new InteractionCriteria(json);
-			assertTrue(criteria.isMet(getTargetContext()));
-		} catch (JSONException e) {
-			Log.e("Error parsing test JSON.", e);
-			assertNull(e);
-		}
+		doTest("testOperatorNot.json");
 		Log.e("Finished test.");
 	}
 
 	public void testOperatorContains() {
 		Log.e("Running test: testOperatorContains()\n\n");
 		resetDevice();
-
-		String json = loadFileAssetAsString(TEST_DATA_DIR + "testOperatorContains.json");
-		try {
-			PersonManager.storePersonEmail(getTargetContext(), "test@example.com");
-			PersonManager.storePersonAndReturnIt(getTargetContext());
-
-			InteractionCriteria criteria = new InteractionCriteria(json);
-			assertTrue(criteria.isMet(getTargetContext()));
-		} catch (JSONException e) {
-			Log.e("Error parsing test JSON.", e);
-			assertNull(e);
-		}
+		PersonManager.storePersonEmail(getTargetContext(), "test@example.com");
+		PersonManager.storePersonAndReturnIt(getTargetContext());
+		doTest("testOperatorContains.json");
 		Log.e("Finished test.");
 	}
 
 	public void testOperatorStartsWith() {
 		Log.e("Running test: testOperatorStartsWith()\n\n");
 		resetDevice();
-
-		String json = loadFileAssetAsString(TEST_DATA_DIR + "testOperatorStartsWith.json");
-		try {
-
-			PersonManager.storePersonEmail(getTargetContext(), "test@example.com");
-			PersonManager.storePersonAndReturnIt(getTargetContext());
-
-			InteractionCriteria criteria = new InteractionCriteria(json);
-			assertTrue(criteria.isMet(getTargetContext()));
-		} catch (JSONException e) {
-			Log.e("Error parsing test JSON.", e);
-			assertNull(e);
-		}
+		PersonManager.storePersonEmail(getTargetContext(), "test@example.com");
+		PersonManager.storePersonAndReturnIt(getTargetContext());
+		doTest("testOperatorStartsWith.json");
 		Log.e("Finished test.");
 	}
 
 	public void testOperatorEndsWith() {
 		Log.e("Running test: testOperatorEndsWith()\n\n");
 		resetDevice();
+		PersonManager.storePersonEmail(getTargetContext(), "test@example.com");
+		PersonManager.storePersonAndReturnIt(getTargetContext());
+		doTest("testOperatorEndsWith.json");
+		Log.e("Finished test.");
+	}
 
-		String json = loadFileAssetAsString(TEST_DATA_DIR + "testOperatorEndsWith.json");
+	private void doTest(String testFile) {
+		String json = loadFileAssetAsString(TEST_DATA_DIR + testFile);
 		try {
-			PersonManager.storePersonEmail(getTargetContext(), "test@example.com");
-			PersonManager.storePersonAndReturnIt(getTargetContext());
-
 			InteractionCriteria criteria = new InteractionCriteria(json);
 			assertTrue(criteria.isMet(getTargetContext()));
 		} catch (JSONException e) {
 			Log.e("Error parsing test JSON.", e);
 			assertNull(e);
 		}
-		Log.e("Finished test.");
 	}
 }
