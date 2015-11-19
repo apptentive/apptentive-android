c398e0f4cffa311475b62d0846169f7a2e3ad54d
skykelsey <sky.kelsey@gmail.com>
2015-11-18 16:14:07 -0800

    Refactor `ConditionalOperator` so each operator contains the logic required to peform its operation. `ConditionalClause` is simplified as a result.

--- apptentive/src/com/apptentive/android/sdk/module/engagement/logic/ConditionalClause.java
+++ apptentive/src/com/apptentive/android/sdk/module/engagement/logic/ConditionalClause.java
@@ -8,13 +8,11 @@ package com.apptentive.android.sdk.module.engagement.logic;
 
 import android.content.Context;
 
-import com.apptentive.android.sdk.Apptentive;
 import com.apptentive.android.sdk.Log;
 import com.apptentive.android.sdk.util.Util;
 
 import org.json.JSONObject;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -69,175 +67,10 @@ public class ConditionalClause implements Clause {
 	public boolean evaluate(Context context) {
 		Log.v("    - %s", query);
 		Comparable field = FieldManager.getValue(context, query);
-		Log.v("      - => %s", Util.classToString(field));
 		for (ConditionalTest test : conditionalTests) {
-			switch (test.operator) {
-				case $exists: {
-					Log.v("      - %s %s?", test.operator, Util.classToString(test.parameter));
-					if (test.parameter == null) {
-						return false;
-					}
-					if (!(test.parameter instanceof Boolean)) {
-						return false;
-					}
-					boolean exists = FieldManager.exists(context, query);
-					boolean parameter = (Boolean) test.parameter;
-					if (exists == parameter) {
-						continue;
-					} else {
-						return false;
-					}
-				}
-				case $eq:
-					Log.v("      - %s %s?", test.operator, Util.classToString(test.parameter));
-					if (field == null && test.parameter == null) {
-						continue;
-					}
-					if (field == null || test.parameter == null) {
-						return false;
-					}
-					if (field.getClass() != test.parameter.getClass()) {
-						return false;
-					}
-					if (field.compareTo(test.parameter) == 0) {
-						continue;
-					} else {
-						return false;
-					}
-				case $ne:
-					Log.v("      - %s %s?", test.operator, Util.classToString(test.parameter));
-					if (field == null || test.parameter == null) {
-						return false;
-					}
-					if (field.getClass() != test.parameter.getClass()) {
-						return false;
-					}
-					if (field.compareTo(test.parameter) != 0) {
-						continue;
-					} else {
-						return false;
-					}
-				case $lt:
-					Log.v("      - %s %s?", test.operator, Util.classToString(test.parameter));
-					if (field == null || test.parameter == null) {
-						return false;
-					}
-					if (field.getClass() != test.parameter.getClass()) {
-						return false;
-					}
-					if (field.compareTo(test.parameter) < 0) {
-						continue;
-					} else {
-						return false;
-					}
-				case $lte:
-					Log.v("      - %s %s?", test.operator, Util.classToString(test.parameter));
-					if (field == null || test.parameter == null) {
-						return false;
-					}
-					if (field.getClass() != test.parameter.getClass()) {
-						return false;
-					}
-					if (field.compareTo(test.parameter) <= 0) {
-						continue;
-					} else {
-						return false;
-					}
-				case $gte:
-					Log.v("      - %s %s?", test.operator, Util.classToString(test.parameter));
-					if (field == null || test.parameter == null) {
-						return false;
-					}
-					if (field.getClass() != test.parameter.getClass()) {
-						return false;
-					}
-					if (field.compareTo(test.parameter) >= 0) {
-						continue;
-					} else {
-						return false;
-					}
-				case $gt:
-					Log.v("      - %s %s?", test.operator, Util.classToString(test.parameter));
-					if (field == null || test.parameter == null) {
-						return false;
-					}
-					if (field.getClass() != test.parameter.getClass()) {
-						return false;
-					}
-					if (field.compareTo(test.parameter) > 0) {
-						continue;
-					} else {
-						return false;
-					}
-				case $starts_with:
-					Log.v("      - %s %s?", test.operator, Util.classToString(test.parameter));
-					if (!(field instanceof String) || !(test.parameter instanceof String)) {
-						return false;
-					}
-					if (((String) field).toLowerCase().startsWith(((String) test.parameter).toLowerCase())) {
-						continue;
-					} else {
-						return false;
-					}
-				case $ends_with:
-					Log.v("      - %s %s?", test.operator, Util.classToString(test.parameter));
-					if (!(field instanceof String) || !(test.parameter instanceof String)) {
-						return false;
-					}
-					if (((String) field).toLowerCase().endsWith(((String) test.parameter).toLowerCase())) {
-						continue;
-					} else {
-						return false;
-					}
-				case $contains:
-					Log.v("      - %s %s?", test.operator, Util.classToString(test.parameter));
-					if (field == null || test.parameter == null) {
-						return false;
-					}
-					if (!(field instanceof String) || !(test.parameter instanceof String)) {
-						return false;
-					}
-					if (((String) field).toLowerCase().contains(((String) test.parameter).toLowerCase())) {
-						continue;
-					} else {
-						return false;
-					}
-				case $before: {
-					// The parameter for $before is an offset in seconds added to the current time.
-					if (!(test.parameter instanceof BigDecimal)) {
-						return false;
-					}
-					if (!(field instanceof Apptentive.DateTime)) {
-						return false;
-					}
-					Double offset = ((BigDecimal) test.parameter).doubleValue();
-					Double currentTime = Util.currentTimeSeconds();
-					Apptentive.DateTime offsetDateTime = new Apptentive.DateTime(currentTime + offset);
-					Log.v("      - %s %s?", test.operator, Util.classToString(offsetDateTime));
-					if (((Apptentive.DateTime) field).compareTo(offsetDateTime) < 0) {
-						continue;
-					} else {
-						return false;
-					}
-				}
-				case $after: {
-					// The parameter for $after is an offset in seconds added to the current time.
-					if (!(test.parameter instanceof BigDecimal)) {
-						return false;
-					}
-					if (!(field instanceof Apptentive.DateTime)) {
-						return false;
-					}
-					Double offset = ((BigDecimal) test.parameter).doubleValue();
-					Double currentTime = Util.currentTimeSeconds();
-					Apptentive.DateTime offsetDateTime = new Apptentive.DateTime(currentTime + offset);
-					Log.v("      - %s %s?", test.operator, Util.classToString(offsetDateTime));
-					if (((Apptentive.DateTime) field).compareTo(offsetDateTime) > 0) {
-						continue; // The compiler says this is unnecessary. But you just know that if you remove it, you won't remember to add it again when you add support for another operator.
-					} else {
-						return false;
-					}
-				}
+			Log.v("      - %s %s %s?", Util.classToString(field), test.operator, Util.classToString(test.parameter));
+			if (!test.operator.apply(field, test.parameter)) {
+				return false;
 			}
 		}
 		return true;
--- apptentive/src/com/apptentive/android/sdk/module/engagement/logic/ConditionalOperator.java
+++ apptentive/src/com/apptentive/android/sdk/module/engagement/logic/ConditionalOperator.java
@@ -6,28 +6,179 @@
 
 package com.apptentive.android.sdk.module.engagement.logic;
 
+import com.apptentive.android.sdk.Apptentive;
+import com.apptentive.android.sdk.Log;
+import com.apptentive.android.sdk.util.Util;
+
+import java.math.BigDecimal;
+
 /**
  * @author Sky Kelsey
  */
 public enum ConditionalOperator {
-	$exists,
+	$exists {
+		@Override
+		public boolean apply(Comparable first, Comparable second) {
+			if (second == null) {
+				return false;
+			}
+			if (!(second instanceof Boolean)) {
+				return false;
+			}
+			boolean exists = first != null;
+			return exists == (Boolean) second;
+		}
+	},
 
-	$ne,
-	$eq,
+	$ne {
+		@Override
+		public boolean apply(Comparable first, Comparable second) {
+			if (first == null || second == null) {
+				return false;
+			}
+			if (first.getClass() != second.getClass()) {
+				return false;
+			}
+			return first.compareTo(second) != 0;
+		}
+	},
+	$eq {
+		@Override
+		public boolean apply(Comparable first, Comparable second) {
+			if (first == null && second == null) {
+				return true;
+			}
+			if (first == null || second == null) {
+				return false;
+			}
+			if (first.getClass() != second.getClass()) {
+				return false;
+			}
+			return first.compareTo(second) == 0;
+		}
+	},
 
-	$lt,
-	$lte,
-	$gte,
-	$gt,
+	$lt {
+		@Override
+		public boolean apply(Comparable first, Comparable second) {
+			if (first == null || second == null) {
+				return false;
+			}
+			if (first.getClass() != second.getClass()) {
+				return false;
+			}
+			return first.compareTo(second) < 0;
+		}
+	},
+	$lte {
+		@Override
+		public boolean apply(Comparable first, Comparable second) {
+			if (first == null || second == null) {
+				return false;
+			}
+			if (first.getClass() != second.getClass()) {
+				return false;
+			}
+			return first.compareTo(second) <= 0;
+		}
+	},
+	$gte {
+		@Override
+		public boolean apply(Comparable first, Comparable second) {
+			if (first == null || second == null) {
+				return false;
+			}
+			if (first.getClass() != second.getClass()) {
+				return false;
+			}
+			return first.compareTo(second) >= 0;
+		}
+	},
+	$gt {
+		@Override
+		public boolean apply(Comparable first, Comparable second) {
+			if (first == null || second == null) {
+				return false;
+			}
+			if (first.getClass() != second.getClass()) {
+				return false;
+			}
+			return first.compareTo(second) > 0;
+		}
+	},
 
-	$contains,
-	$starts_with,
-	$ends_with,
+	$contains {
+		@Override
+		public boolean apply(Comparable first, Comparable second) {
+			if (first == null || second == null) {
+				return false;
+			}
+			if (!(first instanceof String) || !(second instanceof String)) {
+				return false;
+			}
+			return ((String) first).toLowerCase().contains(((String) second).toLowerCase());
+		}
+	},
+	$starts_with {
+		@Override
+		public boolean apply(Comparable first, Comparable second) {
+			if (!(first instanceof String) || !(second instanceof String)) {
+				return false;
+			}
+			return ((String) first).toLowerCase().startsWith(((String) second).toLowerCase());
+		}
+	},
+	$ends_with {
+		@Override
+		public boolean apply(Comparable first, Comparable second) {
+			if (!(first instanceof String) || !(second instanceof String)) {
+				return false;
+			}
+			return ((String) first).toLowerCase().endsWith(((String) second).toLowerCase());
+		}
+	},
 
-	$before,
-	$after,
+	$before {
+		@Override
+		public boolean apply(Comparable first, Comparable second) {
+			if (!(first instanceof Apptentive.DateTime)) {
+				return false;
+			}
+			// The parameter for $before is an offset in seconds added to the current time.
+			if (!(second instanceof BigDecimal)) {
+				return false;
+			}
+			Double offset = ((BigDecimal) second).doubleValue();
+			Double currentTime = Util.currentTimeSeconds();
+			Apptentive.DateTime offsetDateTime = new Apptentive.DateTime(currentTime + offset);
+			Log.v("      		- %s?", Util.classToString(offsetDateTime));
+			return ((Apptentive.DateTime) first).compareTo(offsetDateTime) < 0;
+		}
+	},
+	$after {
+		@Override
+		public boolean apply(Comparable first, Comparable second) {
+			if (!(first instanceof Apptentive.DateTime)) {
+				return false;
+			}
+			// The parameter for $after is an offset in seconds added to the current time.
+			if (!(second instanceof BigDecimal)) {
+				return false;
+			}
+			Double offset = ((BigDecimal) second).doubleValue();
+			Double currentTime = Util.currentTimeSeconds();
+			Apptentive.DateTime offsetDateTime = new Apptentive.DateTime(currentTime + offset);
+			Log.v("      		- %s?", Util.classToString(offsetDateTime));
+			return ((Apptentive.DateTime) first).compareTo(offsetDateTime) > 0;
+		}
+	},
 
-	unknown;
+	unknown {
+		@Override
+		public boolean apply(Comparable first, Comparable second) {
+			return false;
+		}
+	};
 
 	public static ConditionalOperator parse(String name) {
 		if (name != null) {
@@ -40,4 +191,5 @@ public enum ConditionalOperator {
 		return unknown;
 	}
 
+	public abstract boolean apply(Comparable first, Comparable second);
 }
