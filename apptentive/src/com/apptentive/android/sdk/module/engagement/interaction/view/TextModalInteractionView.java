0d2e3fe63b045f3428538dd9d7f149e8b3653ae8
skykelsey <sky.kelsey@gmail.com>
2015-11-30 17:22:41 -0800

    Fix potential NPE reported by Infer.

--- apptentive/src/com/apptentive/android/sdk/module/engagement/interaction/view/TextModalInteractionView.java
+++ apptentive/src/com/apptentive/android/sdk/module/engagement/interaction/view/TextModalInteractionView.java
@@ -21,6 +21,7 @@ import com.apptentive.android.sdk.module.engagement.interaction.model.Interactio
 import com.apptentive.android.sdk.module.engagement.interaction.model.Invocation;
 import com.apptentive.android.sdk.module.engagement.interaction.model.TextModalInteraction;
 import com.apptentive.android.sdk.module.engagement.interaction.model.common.Action;
+import com.apptentive.android.sdk.module.engagement.interaction.model.common.Actions;
 import com.apptentive.android.sdk.module.engagement.interaction.model.common.LaunchInteractionAction;
 import com.apptentive.android.sdk.module.engagement.interaction.view.common.ApptentiveDialogButton;
 
@@ -63,8 +64,12 @@ public class TextModalInteractionView extends InteractionView<TextModalInteracti
 		}
 
 		LinearLayout bottomArea = (LinearLayout) activity.findViewById(R.id.bottom_area);
-		List<Action> actions = interaction.getActions().getAsList();
+		Actions actionsObject = interaction.getActions();
 		boolean vertical;
+		List<Action> actions = null;
+		if (actionsObject != null) {
+			actions = actionsObject.getAsList();
+		}
 		if (actions != null && !actions.isEmpty()) {
 			int totalChars = 0;
 			for (Action button : actions) {
