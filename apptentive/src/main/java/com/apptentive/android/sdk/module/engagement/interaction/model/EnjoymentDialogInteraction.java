ce045004da8b61a695f5824c1d80a80c4e04eedd
skykelsey <sky.kelsey@gmail.com>
2016-05-05 17:49:50 -0700

    Add a cancel button to the Enjoyment Dialog. Turns on if configured in the interaction, and applies contentDescription from server as well.

--- apptentive/src/main/java/com/apptentive/android/sdk/module/engagement/interaction/model/EnjoymentDialogInteraction.java
+++ apptentive/src/main/java/com/apptentive/android/sdk/module/engagement/interaction/model/EnjoymentDialogInteraction.java
@@ -19,6 +19,8 @@ public class EnjoymentDialogInteraction extends Interaction {
 	private static final String KEY_TITLE = "title";
 	private static final String KEY_YES_TEXT = "yes_text";
 	private static final String KEY_NO_TEXT = "no_text";
+	private static final String KEY_SHOW_DISMISS_BUTTON = "show_dismiss_button";
+	private static final String KEY_DISMISS_TEXT = "dismiss_text";
 
 	public EnjoymentDialogInteraction(String json) throws JSONException {
 		super(json);
@@ -47,5 +49,21 @@ public class EnjoymentDialogInteraction extends Interaction {
 			return configuration.optString(KEY_NO_TEXT, null);
 		}
 		return null;
+	}
+
+	public boolean showDismissButton() {
+		InteractionConfiguration configuration = getConfiguration();
+		if (configuration == null) {
+			return false;
+		}
+		return configuration.optBoolean(KEY_SHOW_DISMISS_BUTTON, false);
+	}
+
+	public String getDismissText() {
+		InteractionConfiguration configuration = getConfiguration();
+		if (configuration == null) {
+			return null;
+		}
+		return configuration.optString(KEY_DISMISS_TEXT, null);
 	}
 }
