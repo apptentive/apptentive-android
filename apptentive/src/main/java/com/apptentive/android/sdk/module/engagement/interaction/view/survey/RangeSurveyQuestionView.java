cb3fa7074af8f0a402f04d047eb458cde9d172b4
skykelsey <sky.kelsey@gmail.com>
2016-06-15 16:49:26 -0700

    Add radio buttons and all labels to range question type. ANDROID-694

--- apptentive/src/main/java/com/apptentive/android/sdk/module/engagement/interaction/view/survey/RangeSurveyQuestionView.java
+++ apptentive/src/main/java/com/apptentive/android/sdk/module/engagement/interaction/view/survey/RangeSurveyQuestionView.java
@@ -7,16 +7,23 @@
 package com.apptentive.android.sdk.module.engagement.interaction.view.survey;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.apptentive.android.sdk.ApptentiveLog;
 import com.apptentive.android.sdk.R;
 import com.apptentive.android.sdk.module.engagement.interaction.model.survey.RangeQuestion;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.text.NumberFormat;
 
 
 public class RangeSurveyQuestionView extends BaseSurveyQuestionView<RangeQuestion> {
@@ -44,7 +51,42 @@ public class RangeSurveyQuestionView extends BaseSurveyQuestionView<RangeQuestio
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = super.onCreateView(inflater, container, savedInstanceState);
-		inflater.inflate(R.layout.apptentive_survey_question_range, getAnswerContainer(v));
+		ViewGroup answerContainer = getAnswerContainer(v);
+		ViewGroup answer = (ViewGroup) inflater.inflate(R.layout.apptentive_survey_question_range_answer, answerContainer, false);
+		answerContainer.addView(answer);
+
+		String minLabel = question.getMinLabel();
+		if (!TextUtils.isEmpty(minLabel)) {
+			TextView minLabelTextView = (TextView) answer.findViewById(R.id.min_label);
+			minLabelTextView.setText(minLabel);
+		}
+		String maxLabel = question.getMaxLabel();
+		if (!TextUtils.isEmpty(maxLabel)) {
+			TextView maxLabelTextView = (TextView) answer.findViewById(R.id.max_label);
+			maxLabelTextView.setText(maxLabel);
+		}
+
+		LinearLayout rangeContainer = (LinearLayout) answer.findViewById(R.id.range_container);
+		int min = question.getMin();
+		int max = question.getMax();
+
+		NumberFormat defaultNumberFormat = NumberFormat.getInstance();
+
+		for (int i = min; i <= max; i++) {
+			try {
+				CompoundButton compoundButton = (CompoundButton) inflater.inflate(R.layout.apptentive_survey_question_range_choice, rangeContainer, false);
+				compoundButton.setText(defaultNumberFormat.format(i));
+				rangeContainer.addView(compoundButton);
+			} catch (Throwable e) {
+				String message = "Error";
+				while (e != null) {
+					ApptentiveLog.e(message, e);
+					message = " caused by:";
+					e = e.getCause();
+				}
+				throw new RuntimeException(e);
+			}
+		}
 		return v;
 	}
 
