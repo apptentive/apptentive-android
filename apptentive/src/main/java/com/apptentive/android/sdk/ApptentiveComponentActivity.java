ad40d1c47b69a97d5c26ee86c02a52b88d3dd6d1
Alex Lementuev <a.lementuev@gmail.com>
2017-01-20 10:38:50 -0800

    Refactoring
    Removed unused import in ApptentiveComponentActivity.java

--- apptentive/src/main/java/com/apptentive/android/sdk/ApptentiveComponentActivity.java
+++ apptentive/src/main/java/com/apptentive/android/sdk/ApptentiveComponentActivity.java
@@ -1,7 +1,6 @@
 package com.apptentive.android.sdk;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
