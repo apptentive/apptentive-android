f45ed5aed05b187d06063c03643b655e331597c7
skykelsey <sky.kelsey@gmail.com>
2017-01-26 16:51:18 -0800

    Test the rest of `SessionData`
    Need to mock `TextUtils` to make this run on the JVM

--- apptentive/src/test/java/com/apptentive/android/sdk/storage/SessionDataTest.java
+++ apptentive/src/test/java/com/apptentive/android/sdk/storage/SessionDataTest.java
@@ -6,9 +6,18 @@
 
 package com.apptentive.android.sdk.storage;
 
+import android.text.TextUtils;
+
 import com.apptentive.android.sdk.util.Util;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -17,17 +26,53 @@ import java.io.ObjectOutputStream;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(TextUtils.class)
 public class SessionDataTest {
+
+	@Before
+	public void setup() {
+		PowerMockito.mockStatic(TextUtils.class);
+		PowerMockito.when(TextUtils.isEmpty(any(CharSequence.class))).thenAnswer(new Answer<Boolean>() {
+			@Override
+			public Boolean answer(InvocationOnMock invocation) throws Throwable {
+				CharSequence a = (CharSequence) invocation.getArguments()[0];
+				return !(a != null && a.length() > 0);
+			}
+		});
+	}
 
 	@Test
 	public void testSerialization() {
-		String conversationId = "jvnuveanesndndnadldbj";
-		String conversationToken = "watgsiovncsagjmcneiusdolnfcs";
-
 		SessionData expected = new SessionData();
-		expected.setConversationId(conversationId);
-		expected.setConversationToken(conversationToken);
+		expected.setConversationId("jvnuveanesndndnadldbj");
+		expected.setConversationToken("watgsiovncsagjmcneiusdolnfcs");
+		expected.setPersonId("sijngmkmvewsnblkfmsd");
+		expected.setPersonEmail("nvuewnfaslvbgflkanbx");
+		expected.setPersonName("fslkgkdsnbnvwasdibncksd");
+		expected.setLastSeenSdkVersion("mdvnjfuoivsknbjgfaoskdl");
+		expected.setMessageCenterFeatureUsed(true);
+		expected.setMessageCenterWhoCardPreviouslyDisplayed(false);
+		expected.setMessageCenterPendingMessage("`~!@#$%^&*(_+{}:\"'<>?!@#$%^&*()_+{}|:<>?");
+		expected.setMessageCenterPendingAttachments("NVBUOIVKNBGFANWKSLBJK");
+		expected.setTargets("MNCIUFIENVBFKDV");
+		expected.setInteractions("nkjvdfikjbffasldnbnfldfmfd");
+		expected.setInteractionExpiration(1234567894567890345L);
+
+		/*
+		 // TODO: Test these
+		 private Device device;
+		 private Device lastSentDevice;
+		 private Person person;
+		 private Person lastSentPerson;
+		 private Sdk sdk;
+		 private AppRelease appRelease;
+		 private EventData eventData;
+		 private VersionHistory versionHistory;
+		 */
+
 
 		ByteArrayOutputStream baos = null;
 		ObjectOutputStream oos = null;
@@ -46,6 +91,18 @@ public class SessionDataTest {
 			SessionData result = (SessionData) ois.readObject();
 			assertEquals(expected.getConversationId(), result.getConversationId());
 			assertEquals(expected.getConversationToken(), result.getConversationToken());
+			assertEquals(expected.getPersonId(), result.getPersonId());
+			assertEquals(expected.getPersonName(), result.getPersonName());
+			assertEquals(expected.getPersonEmail(), result.getPersonEmail());
+			assertEquals(expected.getLastSeenSdkVersion(), result.getLastSeenSdkVersion());
+			assertEquals(expected.isMessageCenterFeatureUsed(), result.isMessageCenterFeatureUsed());
+			assertEquals(expected.isMessageCenterWhoCardPreviouslyDisplayed(), result.isMessageCenterWhoCardPreviouslyDisplayed());
+			assertEquals(expected.getMessageCenterPendingMessage(), result.getMessageCenterPendingMessage());
+			assertEquals(expected.getMessageCenterPendingAttachments(), result.getMessageCenterPendingAttachments());
+			assertEquals(expected.getTargets(), result.getTargets());
+			assertEquals(expected.getInteractions(), result.getInteractions());
+			assertEquals(expected.getInteractionExpiration(), result.getInteractionExpiration());
+
 		} catch (Exception e) {
 			fail(e.getMessage());
 		} finally {
