45f1e4ec2d3ede47cd26e845d1817787f6b30a6f
skykelsey <sky.kelsey@gmail.com>
2017-01-26 18:32:58 -0800

    Add test for `SessionData` listeners

--- apptentive/src/test/java/com/apptentive/android/sdk/storage/SessionDataTest.java
+++ apptentive/src/test/java/com/apptentive/android/sdk/storage/SessionDataTest.java
@@ -25,6 +25,8 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.Matchers.any;
 
@@ -62,15 +64,7 @@ public class SessionDataTest {
 		expected.setInteractionExpiration(1234567894567890345L);
 
 		/*
-		 // TODO: Test these
-		 private Device device;
-		 private Device lastSentDevice;
-		 private Person person;
-		 private Person lastSentPerson;
-		 private Sdk sdk;
-		 private AppRelease appRelease;
-		 private EventData eventData;
-		 private VersionHistory versionHistory;
+		 // TODO: Test nested objects as well
 		 */
 
 
@@ -111,7 +105,229 @@ public class SessionDataTest {
 			Util.ensureClosed(bais);
 			Util.ensureClosed(ois);
 		}
-
-
 	}
+
+	private boolean listenerFired;
+
+	@Test
+	public void testListeners() {
+
+		DataChangedListener listener = new DataChangedListener() {
+			@Override
+			public void onDataChanged() {
+				listenerFired = true;
+			}
+		};
+
+		SessionData sessionData = new SessionData();
+		sessionData.setDataChangedListener(listener);
+		assertFalse(listenerFired);
+
+		sessionData.setConversationToken("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.setConversationId("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.setPersonId("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.setPersonEmail("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.setPersonName("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.setLastSeenSdkVersion("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.setMessageCenterFeatureUsed(true);
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.setMessageCenterWhoCardPreviouslyDisplayed(true);
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.setMessageCenterPendingMessage("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.setMessageCenterPendingAttachments("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.setInteractions("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.setTargets("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.setInteractionExpiration(1000L);
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+
+		sessionData.getDevice().getCustomData().put("foo", "bar");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getDevice().getCustomData().remove("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.setDevice(new Device());
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getDevice().getCustomData().put("foo", "bar");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getDevice().getIntegrationConfig().setAmazonAwsSns(new IntegrationConfigItem());
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getDevice().setIntegrationConfig(new IntegrationConfig());
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getDevice().getIntegrationConfig().setAmazonAwsSns(new IntegrationConfigItem());
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getDevice().setOsApiLevel(5);
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getDevice().setUuid("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.setLastSentDevice(new Device());
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getLastSentDevice().setUuid("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+
+		sessionData.setPerson(new Person());
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getPerson().setId("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getPerson().setEmail("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getPerson().setName("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getPerson().setFacebookId("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getPerson().setPhoneNumber("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getPerson().setStreet("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getPerson().setCity("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getPerson().setZip("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getPerson().setCountry("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getPerson().setBirthday("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getPerson().setCustomData(new CustomData());
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getPerson().getCustomData().put("foo", "bar");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getPerson().getCustomData().remove("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.setLastSentPerson(new Person());
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getLastSentPerson().setId("foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+
+		sessionData.setSdk(new Sdk());
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.setAppRelease(new AppRelease());
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+
+		sessionData.getVersionHistory().updateVersionHistory(100D, 1, "1");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.setVersionHistory(new VersionHistory());
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getVersionHistory().updateVersionHistory(100D, 1, "1");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+
+		sessionData.getEventData().storeEventForCurrentAppVersion(100D, 10, "1.0", "foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getEventData().storeInteractionForCurrentAppVersion(100D, 10, "1.0", "foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.setEventData(new EventData());
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getEventData().storeEventForCurrentAppVersion(100D, 10, "1.0", "foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+
+		sessionData.getEventData().storeInteractionForCurrentAppVersion(100D, 10, "1.0", "foo");
+		assertTrue(listenerFired);
+		listenerFired = false;
+	}
+
+	// TODO: Add a test for verifying that setting an existing value doesn't fire listeners.
 }
