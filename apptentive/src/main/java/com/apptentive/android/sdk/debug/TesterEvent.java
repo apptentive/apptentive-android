8310fc47aef2acc74ede23de33922d0335b711e8
Alex Lementuev <a.lementuev@gmail.com>
2017-02-04 17:20:13 -0800

    Added ‘Tester’

---
+++ apptentive/src/main/java/com/apptentive/android/sdk/debug/TesterEvent.java
@@ -0,0 +1,6 @@
+package com.apptentive.android.sdk.debug;
+
+public class TesterEvent
+{
+    public static final String EVT_EXCEPTION = "exception"; // { class:String, message:String, stackTrace:String }
+}
