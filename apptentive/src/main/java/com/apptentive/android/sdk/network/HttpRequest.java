6073512a17be71c7abf25e656ac094b91a225e63
Alex Lementuev <a.lementuev@gmail.com>
2017-02-07 15:51:39 -0800

    HttpRequest refactoring
    Removed some unused code and added more traces

--- apptentive/src/main/java/com/apptentive/android/sdk/network/HttpRequest.java
+++ apptentive/src/main/java/com/apptentive/android/sdk/network/HttpRequest.java
@@ -1,7 +1,6 @@
 package com.apptentive.android.sdk.network;
 
 import com.apptentive.android.sdk.ApptentiveLog;
-import com.apptentive.android.sdk.util.StringUtils;
 import com.apptentive.android.sdk.util.Util;
 import com.apptentive.android.sdk.util.threading.DispatchQueue;
 import com.apptentive.android.sdk.util.threading.DispatchTask;
@@ -61,11 +60,6 @@ public class HttpRequest {
 	private String urlString;
 
 	/**
-	 * Optional request URL query params
-	 */
-	private Map<String, Object> queryParams;
-
-	/**
 	 * Request HTTP properties (will be added to a connection)
 	 */
 	private Map<String, Object> requestProperties;
@@ -119,11 +113,6 @@ public class HttpRequest {
 	private Listener listener;
 
 	/**
-	 * Total request durationMillis in milliseconds
-	 */
-	private long durationMillis;
-
-	/**
 	 * Optional dispatch queue for listener callbacks
 	 */
 	private DispatchQueue callbackQueue;
@@ -174,6 +163,8 @@ public class HttpRequest {
 	// Request async task
 
 	void dispatchSync() {
+		long requestStartTime = System.currentTimeMillis();
+
 		try {
 			sendRequestSync();
 		} catch (Exception e) {
@@ -182,6 +173,8 @@ public class HttpRequest {
 				ApptentiveLog.e(e, "Unable to perform request");
 			}
 		}
+
+		ApptentiveLog.d(NETWORK, "Request finished in %d ms", System.currentTimeMillis() - requestStartTime);
 
 		// use custom callback queue (if any)
 		if (callbackQueue != null) {
@@ -198,9 +191,7 @@ public class HttpRequest {
 
 	private void sendRequestSync() throws IOException {
 		try {
-			long startTimestamp = System.currentTimeMillis();
-
-			URL url = createUrl(urlString);
+			URL url = new URL(urlString);
 			ApptentiveLog.d(NETWORK, "Performing request: %s", url);
 
 			connection = (HttpURLConnection) url.openConnection();
@@ -260,9 +251,6 @@ public class HttpRequest {
 
 			// optionally handle response data (should be overridden in a sub class)
 			handleResponse(responseContent);
-
-			// calculate total duration
-			durationMillis = System.currentTimeMillis() - startTimestamp;
 		} finally {
 			closeConnection();
 		}
@@ -341,42 +329,6 @@ public class HttpRequest {
 
 	//endregion
 
-	//region Query
-
-	/**
-	 * Sets a URL-parameter. The actual usage depends on request method (GET, POST, etc)
-	 */
-	public void setQueryParam(String key, Object value) {
-		if (value != null) {
-			if (queryParams == null) {
-				queryParams = new HashMap<>();
-			}
-			queryParams.put(key, value);
-		}
-	}
-
-	/**
-	 * Sets URL-parameters. The actual usage depends on request method (GET, POST, etc)
-	 */
-	public void setQueryParams(Map<String, Object> queryParams) {
-		if (queryParams != null && queryParams.size() > 0) {
-			Set<Entry<String, Object>> entries = queryParams.entrySet();
-			for (Entry<String, Object> e : entries) {
-				String key = e.getKey();
-				if (key == null) {
-					throw new IllegalArgumentException("Can't add request param: key is null");
-				}
-
-				Object value = e.getValue();
-				if (value != null) {
-					setQueryParam(key, value);
-				}
-			}
-		}
-	}
-
-	//endregion
-
 	//region HTTP request properties
 
 	/**
@@ -389,23 +341,6 @@ public class HttpRequest {
 			}
 			requestProperties.put(key, value);
 		}
-	}
-
-	//endregion
-
-	//region Helpers
-
-	private URL createUrl(String baseUrl) throws IOException {
-		if (HttpRequestMethod.GET.equals(method)) {
-			if (queryParams != null && queryParams.size() > 0) {
-				String query = StringUtils.createQueryString(queryParams);
-				if (baseUrl.endsWith("/")) {
-					return new URL(baseUrl + query);
-				}
-				return new URL(baseUrl + "/" + query);
-			}
-		}
-		return new URL(baseUrl);
 	}
 
 	//endregion
@@ -458,10 +393,6 @@ public class HttpRequest {
 
 	public void setCallbackQueue(DispatchQueue callbackQueue) {
 		this.callbackQueue = callbackQueue;
-	}
-
-	public long duration() {
-		return durationMillis;
 	}
 
 	/**
